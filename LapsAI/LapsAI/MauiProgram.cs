6594bcfbf4afdc3f20b8d0638e5492781bdd9a8e
Balakumara21 <balakumar.a21@gmail.com>
2025-11-26 19:52:15 +0530

    Add project files.

---
+++ LapsAI/LapsAI/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿
+using LapsAI.Services;
+using LapsAI.Shared.Services;
+using Microsoft.Extensions.Logging;
+
+namespace LapsAI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            // Add device-specific services used by the LapsAI.Shared project
+            builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+            //builder.Services.AddCorrelationId();
+
+            builder.Services.AddScoped<StorageService>();
+
+            builder.Services.AddTransient<TokenHandler>();
+
+
+            
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
